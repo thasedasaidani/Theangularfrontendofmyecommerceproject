a879fa1b9e4fa8f87cb0e48a0c8e4670ce682120
thaseda saidani <thasedasaidani2000@gmail.com>
2025-02-25 01:07:03 +0100

    Update the ProductListComponent

--- src/app/common/product.ts
+++ src/app/common/product.ts
@@ -1,2 +1,17 @@
 export class Product {
+    
+    constructor(
+        public  sku: string,
+        public  name: string,
+        public  description: string,
+        public  unitPrice: number,
+        public  active: boolean,
+        public  unitsInStock: string,
+        public dateCreated: Date,
+        public lastUpdate: Date,
+     
+
+    ){
+
+    }
 }
--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../common/product';
+import { ProductService } from '../../services/product.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+
+  products: Product[] = [];
+
+  constructor(private ProductService: ProductService){
+
+  }
+  ngOnInit(): void {
+     this.listProducts();
+  }
+   listProducts() {
+     this.ProductService.getProductList().subscribe(
+       data => {
+        this.products = data;
+       }
+     )
+   }
 
 }
